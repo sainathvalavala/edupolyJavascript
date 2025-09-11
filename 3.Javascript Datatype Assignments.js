7aa7749c26a2e600eed762f7d43c4cc511b34d79
SaiNath <146621032+sainathvalavala@users.noreply.github.com>
2025-09-11 22:47:29 +0530

    Update 3.Javascript Datatype Assignments.js

--- 3.Javascript Datatype Assignments.js
+++ 3.Javascript Datatype Assignments.js
@@ -1,22 +1,28 @@
 //  Javascript Datatype Assignments 
-
-// 1. Create a variable with integer value 
-
-// 2. Create a variable and assign octal number 
-
-// 3. Create a variable and assign hexadecimal value 
-
+// 1. Create a variable with integer value
+let a = 42;
+// 2. Create a variable and assign octal number
+// In modern JavaScript, octal literals use a leading 0o or 0O
+let b = 0o52;        
+// 3. Create a variable and assign hexadecimal value
+// Hexadecimal literals use 0x or 0X
+let c = 0x2A;         
 // 4. Create a variable and assign Boolean value 
-    var x= true;
-    console.log(x)
+ var x= true;
 // 5. Create a variable and assign string value 
- let x = "Shelby";
+ let y = "Shelby";
 // 6. Create a variable and assign undefined value 
+let z= undefined;
 
 // 7. Create a variable and assign null value 
+let a = null;
 
 // 8. Create a variable and assign NaN value 
+let b = NaN;
 
 // 9. Create a variable and assign array 
+let c = [ "sai", 1 , null, 2, "Shelby"]
 
 // 10. Create a variable and assign object literal 
+let d={ age: 23, name : "shelby", gender : "male", nationality : "india"}
+
